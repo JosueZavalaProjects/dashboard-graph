59cc30265a6ad901bd17512c92cd6604fc3b3b04
Josue Zavala <josue.zavala@wildfork.mx>
2025-06-25 14:39:35 -0600

    feat(Dashboard): Adding 3 Bars icon

--- src/components/Navigation/NavBar/Navbar.tsx
+++ src/components/Navigation/NavBar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Bars3Icon } from "@heroicons/react/16/solid";
 import { NavItem } from "./NavItem";
 
 interface Props {
@@ -9,7 +10,9 @@ export const NavBar = ({ children }: Props) => {
     <section className="flex items-center justify-center p-8 bg-slate-200 w-screen h-screen mx-auto">
       <main className="flex h-full w-[70rem] rounded-xl bg-white">
         <div className="flex flex-col gap-4 rounded-l-xl bg-slate-100 p-6 w-1/5 border-r border-r-slate-300">
-          <div>hamburger icon</div>
+          <span>
+            <Bars3Icon className="size-10 text-gray-700" />
+          </span>
           <nav>
             <ul className="flex flex-col gap-2 text-lg">
               <NavItem label="Dashboard" isSelected />
